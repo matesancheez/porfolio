596fdeafd8f815a11fd497fcd8099c3a05b2e6a4
Mateo <sanchezmateo090@gmail.com>
2023-09-08 18:00:38 -0300

    Uploading full page

---
+++ src/data/info.ts
@@ -0,0 +1,90 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Mateo Sanchez",
+  jobDescription: "Web Developer",
+  about: `I'm currently in my final year as a university systems information analyst. I have a passion for web development and have been honing my skills in various technologies. My toolkit includes HTML, CSS, Sass, JavaScript, React, Python, and Django. I'm also familiar with Bootstrap, Tailwind CSS, and have some basic knowledge of Astro.
+  Beyond web development, I have experience working with different programming paradigms and have a solid understanding of various operating systems like Kali Linux, Ubuntu and Window. I also have an advanced understanding of algorithms and data structures.
+  I'm enthusiastic about crafting digital experiences and continually strive to expand my skillset.
+  `,
+
+  experience: [
+    
+
+    {
+      name: "V&S Odontologia",
+      location: "Salta, Argentina",
+      startDate: "Dic 2020",
+      endDate: "Apr 2021",
+      description: [
+        "✓ In charge of modeling the business to build accurate software",
+        "✓ Build the UI for a dental office and its patients",
+        "✓ Devise solutions to make the work of doctors easier",
+        "✓ Obsessive with good practices and reusable code",
+      ],
+    },
+
+  ],
+
+  education: [
+    {
+      name: "Universidad Tecnologica Nacional FRC",
+      location: "Cordoba, Cordoba, Argentina",
+      startDate: "2021",
+      endDate: "Now",
+      description: [
+        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+      ],
+    },
+    {
+      name: "Coder House",
+      location: "Online",
+      startDate: "2020",
+      endDate: "2020",
+      description: ["Couse, Web Development with React"],
+    },
+    {
+      name: "Coder House",
+      location: "Online",
+      startDate: "2021",
+      endDate: "2021",
+      description: ["Couse, Python and Django backend"],
+    },
+    {
+      name: "Uzzi College",
+      location: "Salta, Argentina",
+      startDate: "2013",
+      endDate: "2018",
+      description: ["Bachelor,  Economy"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/matesancheez",
+    email: "matesanchz@7gmail.com",
+    linkedin: "https://www.linkedin.com/in/mateo-sanchez-40a3761a2/",
+  },
+
+  projects: [
+    {
+      title: "V&S Odontologia",
+      isFeatured: true,
+      thumbnail: "/assets/images/pt.png",
+      githubUrl: "https://github.com/matesancheez/vsodontologia",
+      liveUrl: "https://matesancheez.github.io/vsodontologia/",
+    },
+    {
+      title: "MiAbu",
+      isFeatured: true,
+      thumbnail: "/assets/images/miabu.png",
+      githubUrl: "https://github.com/matesancheez/MiAbu",
+      liveUrl: "https://miabu-production.up.railway.app/",
+    },
+    {
+      title: "Projects React",
+      isFeatured: true,
+      thumbnail: "/assets/images/github.jpg",
+      githubUrl: "https://github.com/matesancheez/Proyectos_React",
+      liveUrl: "https://github.com/matesancheez/Proyectos_React",
+    },
+  ],
+};
